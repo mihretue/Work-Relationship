8ccb2be2b45fe86c14591874c24526f46f5df50e
BEKANCY7 <berekettegene4@gmail.com>
2025-01-27 16:54:03 +0300

    login ui

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import "../styles/LoginForm.css"; // Import a CSS file for styling
+import "../styles/LoginForm.css"; // Ensure this path is correct
 
 const LoginForm = () => {
   const { loading } = useContext(AuthContext);
