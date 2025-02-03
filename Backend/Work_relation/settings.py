fe4260094daf8251911e6e4a12be7de0bbf7480d
mihretue <mihretuendeshaw84@gmail.com>
2025-02-03 21:50:33 +0300

    prepare django for render deployment

--- Backend/Work_relation/settings.py
+++ Backend/Work_relation/settings.py
@@ -67,7 +67,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
+    
 ]
 
 ROOT_URLCONF = 'Work_relation.urls'
